e73cd1887008a754b71c0b36302ff2d2f4cf3072
Tomasz Kalkosiński <tomasz.kalkosinski@gmail.com>
2014-04-13 01:32:22 +0200

    Solved small in war game

--- googlecodejam/2014/qualification/deceitfulwar/deceitfulWar.go
+++ googlecodejam/2014/qualification/deceitfulwar/deceitfulWar.go
@@ -15,7 +15,8 @@ const Debug = false
 //https://code.google.com/codejam/contest/351101/dashboard#s=p0
 func main() {
     log("https://code.google.com/codejam/contest/351101/dashboard#s=p0")
-    input, err := readFlatInput("/home/tkl/dev/puzzles/googlecodejam/2014/qualification/deceitfulwar/sample.in")
+//    input, err := readFlatInput("/home/tkl/dev/puzzles/googlecodejam/2014/qualification/deceitfulwar/sample.in")
+    input, err := readFlatInput("/home/tkl/dev/puzzles/googlecodejam/2014/qualification/deceitfulwar/D-small-attempt0.in")
     if (err != nil) {
         log("Error %v", err)
         os.Exit(1)
