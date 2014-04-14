0a8cc5c2111003abae4364c47e8302948d37ff4c
Tomasz Kalkosiński <tomasz.kalkosinski@gmail.com>
2014-04-14 11:42:52 +0200

    Refactoring towards reuse later

--- googlecodejam/2014/qualification/deceitfulwar/deceitfulWar.go
+++ googlecodejam/2014/qualification/deceitfulwar/deceitfulWar.go
@@ -1,43 +1,58 @@
 package main
 
 import (
-    "bufio"
-    "os"
-    "bytes"
-    "strconv"
-    "fmt"
-    "time"
+	"bufio"
+	"os"
+	"bytes"
+	"strconv"
+	"fmt"
+	"time"
 	"sort"
+	"strings"
 )
 
-const Debug = false
+const (
+	InputFilename       = "D-small-attempt0"
+	ProblemURL          = "https://code.google.com/codejam/contest/2974486/dashboard#s=p3"
+	CaseInputLinesCount = 3
 
-//https://code.google.com/codejam/contest/351101/dashboard#s=p0
+	InputFilenameExtension  = ".in"
+	OutputFileExtension = ".out"
+)
+
+var (
+	TotalTimeCounter time.Duration = 0
+	OutputBuffer bytes.Buffer
+)
+
 func main() {
-    log("https://code.google.com/codejam/contest/351101/dashboard#s=p0")
-//    input, err := readFlatInput("/home/tkl/dev/puzzles/googlecodejam/2014/qualification/deceitfulwar/sample.in")
-//    input, err := readFlatInput("/home/tkl/dev/puzzles/googlecodejam/2014/qualification/deceitfulwar/D-small-attempt0.in")
-    input, err := readFlatInput("/home/tkl/dev/puzzles/googlecodejam/2014/qualification/deceitfulwar/D-large.in")
-    if (err != nil) {
-        log("Error %v", err)
-        os.Exit(1)
-    }
+	log("Problem URL: %v", ProblemURL)
+	input, err := readFlatInput()
+	if (err != nil) {
+		log("Error %v", err)
+		os.Exit(1)
+	}
 
-    casesCount := int(input[0][0])
-    log("Solving %v cases", casesCount)
-    for i := 0 ; i < casesCount ; i++ {
-        start := time.Now()
-        solveCase(i, input[1 + i * 3: 4 + i * 3])
-        log("Took %v", time.Since(start))
-    }
+	casesCount := int(input[0][0])
+	log("Solving %v cases", casesCount)
+	for i := 0 ; i < casesCount ; i++ {
+		caseData := input[1+i*CaseInputLinesCount: (1+CaseInputLinesCount)+i*CaseInputLinesCount]
+		log("Case #%v data: %v", i+1, caseData)
+		start := time.Now()
+		solveCase(i, caseData)
+		duration := time.Since(start)
+		TotalTimeCounter += duration
+		log("Case #%v took %v, total time is %v\n\n", i+1, duration, TotalTimeCounter)
+	}
+	writeOutFile()
 }
 
 func solveCase(index int, input [][]float64) {
-	log("Solving case %v, with data %v", index, input)
+	//	log("Solving case %v, with data %v", index, input)
 
 	naomi := input[1]
 	ken := input[2]
-	
+
 	cheatNaomi := make([]float64, len(naomi))
 	copy(cheatNaomi, naomi)
 	sort.Float64s(cheatNaomi)
@@ -51,7 +66,7 @@ func solveCase(index int, input [][]float64) {
 	copy(warKen, ken)
 	sort.Float64s(warKen)
 
-	out("Case #%v: %v %v", index + 1, cheat(cheatNaomi, cheatKen), war(warNaomi, warKen))
+	out("Case #%v: %v %v", index+1, cheat(cheatNaomi, cheatKen), war(warNaomi, warKen))
 }
 
 func cheat(naomi, ken []float64) int {
@@ -60,7 +75,7 @@ func cheat(naomi, ken []float64) int {
 	rounds := len(naomi)
 
 	for i := 0; i < rounds; i++ {
-//		maxKen := max(ken)
+		//		maxKen := max(ken)
 		minKen := min(ken, 0)
 
 		minNaomi := min(naomi, minKen)
@@ -147,43 +162,57 @@ func min(floats []float64, lowest float64) float64 {
 }
 
 //http://stackoverflow.com/questions/9862443/golang-is-there-a-better-way-read-a-file-of-integers-into-an-array
-func readFlatInput(path string) ([][]float64, error) {
-    file, err := os.Open(path)
-    if (err != nil) {
-        log("Error opening file: %v", err)
-        os.Exit(1)
-    }
-    scanner := bufio.NewScanner(file)
-    result  := make([][]float64, 0)
-    for scanner.Scan() {
-        words := bytes.Split(scanner.Bytes(), []byte{0x20})
-        float64s := make([]float64, 0, len(words))
+func readFlatInput() ([][]float64, error) {
+	wd, err := os.Getwd()
+	file, err := os.Open(wd + "/" + InputFilename + InputFilenameExtension)
+	if (err != nil) {
+		log("Error opening file: %v", err)
+		panic(1)
+	}
+	scanner := bufio.NewScanner(file)
+	result := make([][]float64, 0)
+	for scanner.Scan() {
+		words := bytes.Split(scanner.Bytes(), []byte{0x20})
+		float64s := make([]float64, 0, len(words))
 
-        for _, word := range words {
-            float64eger, err := strconv.ParseFloat(string(word), 64)
-            if err != nil { return nil, err }
-            float64s = append(float64s, float64eger)
-        }
-        result = append(result, float64s)
-    }
-    return result, nil
+		for _, word := range words {
+			float64eger, err := strconv.ParseFloat(string(word), 64)
+			if err != nil { return nil, err }
+			float64s = append(float64s, float64eger)
+		}
+		result = append(result, float64s)
+	}
+	return result, nil
 }
 
-func log(format string, a ...interface{}) (n int, err error) {
-    if !Debug {
-        return
-    }
-    if (a == nil) {
-        return fmt.Print(format + "\n")
-    } else {
-        return fmt.Printf(format + "\n", a...)
-    }
+func writeOutFile() {
+	wd, err := os.Getwd()
+	file, err := os.Create(wd + "/" + InputFilename + OutputFileExtension)
+	if (err != nil) {
+		log("Error creating file: %v", err)
+		panic(1)
+	}
+	_, err = file.WriteString(strings.TrimSpace(OutputBuffer.String()))
+	if err = file.Close(); err != nil {
+		panic(err)
+	}
 }
 
-func out(format string, a ...interface{}) (n int, err error) {
-    if (a == nil) {
-        return fmt.Print(format + "\n")
-    } else {
-        return fmt.Printf(format + "\n", a...)
-    }
+func log(format string, a ...interface{}) {
+	if (a == nil) {
+		fmt.Print(format + "\n")
+	} else {
+		fmt.Printf(format+"\n", a...)
+	}
+}
+
+func out(format string, a ...interface{}) {
+	var formatted string
+	if (a == nil) {
+		formatted = fmt.Sprint(format + "\n")
+	} else {
+		formatted = fmt.Sprintf(format + "\n", a...)
+	}
+	fmt.Print(formatted)
+	OutputBuffer.WriteString(formatted)
 }
